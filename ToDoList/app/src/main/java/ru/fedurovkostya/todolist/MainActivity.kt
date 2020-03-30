912bd96c4440be13cf2d1eae10217b5859e551bd
Evgeniy <58300599+Lipoffv@users.noreply.github.com>
2020-03-30 14:07:58 +0300

    Update MainActivity.kt

--- ToDoList/app/src/main/java/ru/fedurovkostya/todolist/MainActivity.kt
+++ ToDoList/app/src/main/java/ru/fedurovkostya/todolist/MainActivity.kt
@@ -8,18 +8,17 @@ import android.os.Handler
 import android.view.View
 import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_main.*
-//данный класс отвечает за заставку
+// Класс, отвечающий за заставку приложения.
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //меняем цвет statusBar на акцентный цвет, для того
-        //чтобы сморелось лучше)
+        // Меняем цвет statusBar на colorAccent, чтобы заставка была полной;
         if(android.os.Build.VERSION.SDK_INT >= 21){
             window.statusBarColor = this.resources.getColor(R.color.colorAccent)
         }
-        //заставка появляется на 1 секунда, а затем запускоется другое окно
+        // Заставка появляется на 1 секунду, а затем запускается главное окно;
         Handler().postDelayed({
             startActivity(Intent(this,TaskActivity::class.java))
             finish()
