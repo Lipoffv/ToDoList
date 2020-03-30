09a43b3e437efc131cb238c5855948314865cdc1
Evgeniy <58300599+Lipoffv@users.noreply.github.com>
2020-03-30 14:31:22 +0300

    Update TaskAdapter.kt

--- ToDoList/app/src/main/java/ru/fedurovkostya/todolist/TaskAdapter.kt
+++ ToDoList/app/src/main/java/ru/fedurovkostya/todolist/TaskAdapter.kt
@@ -11,10 +11,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.android.synthetic.main.activity_task.*
 import ru.fedurovkostya.todolist.Other.Task
-//класс служит адаптером между базай данных, списком и пользователем.
+// Класс служит адаптером между базой данных, списком и пользователем.
 class TaskAdapter(val activity: TaskActivity, var list:MutableList<Task>): RecyclerView.Adapter<TaskAdapter.ViewHolder>(){
 
-    //создает сам список
+    // Функция создает сам список.
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
             LayoutInflater.from(activity).inflate(
@@ -24,26 +24,26 @@ class TaskAdapter(val activity: TaskActivity, var list:MutableList<Task>): Recyc
             )
         )
     }
-    //возвращает количество объектов
+    // Возвращает количество объектов;
     override fun getItemCount(): Int {
         return list.size
     }
-    //этот блог кода срабатывает при нажатии на задачу, или при ее создании, отображении
+    // Этот блок кода сработает при нажатии на задачу или при ее создании;
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         setSizeOfTask(activity,holder)
-        //устанвка значений:имя, цвет chaeckbox
+        // Установка значений: имя, цвет checkbox;
         holder.tv_task.text = list[position].name
         if(list[position].color == activity.resources.getColor(R.color.colorAccent)){
             holder.iv_task.setColorFilter(activity.resources.getColor(R.color.colorAccent))
         }
-        //при нажатии на checkbox задача удаляется
+        // При нажатии на checkbox, задача удаляется;
         holder.iv_task.setOnClickListener({
             activity.dbHelper.deleteTask(list[position].id)
             activity.refreshList()
         })
-        //при нажатии на задачу всплывает окно снизу для редоктирования
+        // При нажатии на задачу, появляется окно для ее редактирования;
         holder.ll_task.setOnClickListener({
-            //объявление и инициализация объектов
+            // Объявление и инициализация объектов;
             val dialog = BottomSheetDialog(activity)
             val view = activity.layoutInflater.inflate(R.layout.activity_dialog_edit,null)
             var et_name = view.findViewById<EditText>(R.id.et_dialog_name)
@@ -51,17 +51,17 @@ class TaskAdapter(val activity: TaskActivity, var list:MutableList<Task>): Recyc
             var cb_priority = view.findViewById<CheckBox>(R.id.cb_dialog_priority)
             var iv_neg = view.findViewById<ImageView>(R.id.iv_dialog_neg)
             var tv_pos = view.findViewById<TextView>(R.id.tv_dialog_pos)
-            //устновка значение
+            // Установка значения;
             et_description.setText(list[position].description)
             et_name.setText(list[position].name)
             cb_priority.isChecked = activity.resources.getColor(R.color.colorAccent) == list[position].color
             tv_pos.text = "Сохранить"
             iv_neg.setImageResource(R.drawable.ic_back)
-            //при нажатии на стрелочку назад окно закрывается
+            // При нажатии на стрелочку назад, окно редактирования закрывается;
             iv_neg.setOnClickListener {
                 dialog.dismiss()
             }
-            //при нажатии на копку сохрнить данные записывается в базу данных, а затем окно закрывается также.
+            // При нажатии на кнопку Сохранить, данные записываются в базу данных, а затем окно закрывается;
             tv_pos.setOnClickListener {
                 var task = list[position]
                 task.name = et_name.text.toString()
@@ -81,7 +81,7 @@ class TaskAdapter(val activity: TaskActivity, var list:MutableList<Task>): Recyc
         })
     }
 
-    //Установка высоты задачи, как 1/6 высоты экрана
+    // Установка высоты задачи, как 1/6 части экрана;
     fun setSizeOfTask(activity: TaskActivity, holder: ViewHolder){
         var statusBarHeight = 0
         val resourceId = activity.resources.getIdentifier("status_bar_height", "dimen", "android")
@@ -95,7 +95,7 @@ class TaskAdapter(val activity: TaskActivity, var list:MutableList<Task>): Recyc
         holder.ll_task.getLayoutParams().height = deviceheight
     }
 
-    //объявление и инициализация объектов в разметки задачи
+    // Объявление и инициализации объектов в разметке задачи;
     class ViewHolder(v: View): RecyclerView.ViewHolder(v){
         val tv_task: TextView = v.findViewById(R.id.tv_task)
         val iv_task: ImageView = v.findViewById(R.id.iv_task)
